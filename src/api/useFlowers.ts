1901c40582546e582f400b0e7fe945fa11496e1d
Valentino K <valentino@init.hr>
2022-07-25 16:43:49 +0200

    next page param

--- src/api/useFlowers.ts
+++ src/api/useFlowers.ts
@@ -2,5 +2,14 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { fetchFlowers } from "./fetchFlowers";
 
 export const useFlowers = () => {
-  return useInfiniteQuery(["flowers"], fetchFlowers);
+  return useInfiniteQuery(["flowers"], fetchFlowers, {
+    getNextPageParam: (lastPage: any, pages: any) => {
+      const nextPage = pages.length + 1;
+
+      return lastPage.meta.pagination.current_page <
+        lastPage.meta.pagination.total_pages
+        ? nextPage
+        : undefined;
+    },
+  });
 };
